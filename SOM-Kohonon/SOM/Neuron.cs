4761437b033861b8e53482c7f8be395a45ae28f2
Alexey Khan <vraizen@mail.ru>
2019-07-31 01:35:12 +0300

    Made Self-org-map Kohonon Neural Network

---
+++ SOM-Kohonon/SOM/Neuron.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOM
+{
+    public class Neuron   // создание класса нейрон
+    {
+        public double[] Weights;
+        public int X;
+        public int Y;
+        private int length;
+        private double nf;
+        public Neuron(int x, int y, int length)
+        {
+            X = x;
+            Y = y;
+            this.length = length;
+            nf = 1000 / Math.Log(length);
+        }
+        private double Gauss(Neuron win, int it)  // Распределение Гаусса
+        {
+            double distance = Math.Sqrt(Math.Pow(win.X - X, 2) + Math.Pow(win.Y - Y, 2));
+            return Math.Exp(-Math.Pow(distance, 2) / (Math.Pow(Strength(it), 2)));
+        }
+        private double LearningRate(int it)
+        {
+            return Math.Exp(-it / 1000) * 0.1;
+        }
+        private double Strength(int it)
+        {
+            return Math.Exp(-it / nf) * length;
+        }
+        public double UpdateWeights(double[] pattern, Neuron winner, int it) // обновление весов
+        {
+            double sum = 0;
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                double delta = LearningRate(it) * Gauss(winner, it) * (pattern[i] - Weights[i]);
+                Weights[i] += delta;
+                sum += delta;
+            }
+            return sum / Weights.Length;
+        }
+
+    }
+
+
+}
