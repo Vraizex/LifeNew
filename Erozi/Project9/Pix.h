759438ef9b954a86ff3a7ee674aa0fe5730826da
Vraizex <vraizen@mail.ru>
2019-09-17 16:05:30 +0300

    dilate and eroze level addithion

---
+++ Erozi/Project9/Pix.h
@@ -0,0 +1,74 @@
+#pragma once
+#define _USE_MATH_DEFINES
+#define HARD 1  // Жесткий
+#define SOFT 2  // Мягкий
+#define GARROT 3  // Фильтр Гаррота
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/core.hpp>
+#include "opencv2/video/tracking.hpp"
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+#include <cmath>
+#include <limits>
+#include <stdio.h>
+#include <vector>
+
+
+using namespace std;
+using namespace cv;
+
+class Window
+{
+public:
+	Window(std::string const& title) : _title(title), _is_opened(false) {
+		namedWindow(_title, cv::WINDOW_NORMAL);
+	}
+	void show(cv::Mat const& m, bool wait_flag = false) {
+		imshow(_title, m);
+		_is_opened = true;
+		if (wait_flag) {
+			waitKey(0);
+		}
+	}
+	static void wait(int timeout = 0) {
+		waitKey(timeout);
+	}
+	~Window() {
+		if (_is_opened) {
+			destroyWindow(_title);
+		}
+	}
+protected:
+	std::string _title;
+	bool _is_opened;
+};
+
+
+cv::Mat calc3x3GradientSOBOL(cv::Mat& img);
+Mat dilateXY(Mat& img);
+Mat dilate3X3Y(Mat& img);
+Mat dilateGOA(Mat& img, int k);
+Mat dilateAndErozia(Mat& img, int k, int z);
+Mat EroziaAndDilate(Mat& img, int k, int z);
+Mat dilateEroziaLevel(Mat& img, int k);
+Mat dilateGOAEroze(Mat& img, int k);
+Mat mat_SUMa(Mat& img);
+cv::Mat calcLoG(cv::Mat& img);
+cv::Mat calcLoGDiskret(cv::Mat& img);
+cv::Mat calcLoGDiskretWeights(cv::Mat& img);
+cv::Mat calcLoGDiskretWeightsProg(cv::Mat& img);
+cv::Mat GradientSOBOLBinAndMatrix(Mat& img);
+cv::Mat calc3x3GradientSOBOLBinAndMatrix(cv::Mat& img);
+cv::Mat calc3x3GradientPrevitta(cv::Mat& img);
+cv::Mat calcRobertsa(cv::Mat& img);
+cv::Mat calcHGradient(Mat& img);
+Mat dilateY(Mat& img);
+Mat dilateX(Mat& img);
+cv::Mat cvHaarWavelet(Mat &img, Mat &dst, int NIter);
+pair<cv::Mat, cv::Mat> calc3x3Gradient(cv::Mat& img);
+cv::Mat cvInvHaarWavelet(Mat &src, Mat &dst, int NIter, int SHRINKAGE_TYPE = 0, float SHRINKAGE_T = 50); // Вейвлет-преобразование
+
+
---
+++ Erozi/Project9/main.cpp
@@ -0,0 +1,129 @@
+#include "Pix.h"
+
+
+
+void main()
+{
+	try {
+		setlocale(LC_ALL, "Russian");
+
+		/*string file_path = "C:\\Users\\User\\Downloads\\Lenna.png";*/
+		/*std::string file_path = "C:\\Users\\User\\Downloads\\Telegram Desktop\\DataSet_V\\DataSet_V\\img0_.png";*/
+		std::string file_path = "C:\\Users\\User\\Downloads\\im0_.png";
+		std::string file_path1 = "C:\\Users\\User\\Downloads\\im0_.png";
+		
+		Mat img = imread(file_path, 0);
+		Mat dst = imread(file_path, 0);
+		
+		if (!img.data) // no image loading;
+		{
+			throw std::system_error(errno, std::system_category(), file_path);
+		}
+
+
+
+		//Mat mat_horizontal = calcHGradient(img);
+		//Window win_horizontal("Gradiend Field horizontal");
+		//win_horizontal.show(mat_horizontal);
+
+		//auto mat3x3 = calc3x3Gradient(img);
+		//Window win_mod_3x3("Gradiend Field 3x3 module");
+		//Mat modules;
+		//mat3x3.first.convertTo(modules, -1, 0.8, 30.);
+		//win_mod_3x3.show(modules);
+
+
+
+		
+	
+		Mat mat_Robertsa = calcRobertsa(img);
+		Window win_Robertsa("Robertsa");
+		win_Robertsa.show(mat_Robertsa);
+
+		Mat mat_LoG = calcLoG(img);
+		Window win_LoG("LoG");
+		win_LoG.show(mat_LoG);
+	
+
+
+		Mat mat_DISKRetLOGWegth = calcLoGDiskretWeights(img) ;
+		Window win_DISKRetLOGWegth("DISKRetLOGWegth");
+		win_DISKRetLOGWegth.show(mat_DISKRetLOGWegth);
+
+		Mat mat_DISKRetLOGWegthProg = calcLoGDiskretWeightsProg(img) ;
+		Window win_DISKRetLOGWegthProg("DISKRetLOGWegthProg");
+		win_DISKRetLOGWegthProg.show(mat_DISKRetLOGWegthProg);
+
+		Mat mat_sobel = calc3x3GradientSOBOL(img);
+		Window win_sobol("Sobol");
+		win_sobol.show(mat_sobel);
+
+
+		Mat mat_Pr = calc3x3GradientPrevitta(img);
+		Window win_Previtta("Previtta");
+		win_Previtta.show(mat_Pr);
+
+
+
+		Mat mat_BinX = dilateX(img);
+		Window win_BinX("BinX");
+		win_BinX.show(mat_BinX);
+
+
+
+		Mat mat_BinY = dilateY(img);
+		Window win_BinY("BinY");
+		win_BinY.show(mat_BinY);
+
+
+		Mat mat_SUM =  dilateXY(img);
+		Window win_BinSum("SUMBin");
+		win_BinSum.show(mat_SUM);
+
+
+		Mat mat_SUMa = dilateAndErozia(img, 10, 8) + dilateXY(img);
+		Window win_BinSuma("10 Dilate and 8 Erozia");
+		win_BinSuma.show(mat_SUMa);
+		
+		Mat mat_erozanddite = EroziaAndDilate(img, 1, 15)- dilateEroziaLevel(img,1) - calc3x3GradientSOBOL(img) ;
+		Window win_erozdit("FIRST Erozia and Ditale");
+		win_erozdit.show(mat_erozanddite);
+
+		//Mat mat_New = (img);
+		//Window win_So("New Sobol");
+		//win_So.show(mat_New);
+
+		//Mat mat_SUMa2 = dilateGOA(img, 1) ;
+		//Window win_BinSuma2("SUMBin+ X+Y2");
+		//win_BinSuma2.show(mat_SUMa2);
+
+		//Mat mat_SUMa3 = dilateGOA(img, 2);
+		//Window win_BinSuma3("SUMBin+ X+Y3");
+		//win_BinSuma3.show(mat_SUMa3);
+
+
+		//Mat mat_SUMa4 =  dilateGOA(img,5);
+		//Window win_BinSuma4("SUMBin+ X+Y4");
+		//win_BinSuma4.show(mat_SUMa4);
+
+
+		//Mat mat_SUMa5 = dilateGOA(img, 10);
+		//Window win_BinSuma5("SUMBin+ X+Y 5");
+		//win_BinSuma5.show(mat_SUMa5);
+
+
+	
+
+		Window::wait();
+
+		
+	}
+	catch (std::exception const& ex)
+	{
+		std::cout << "fail: " << ex.what() << std::endl;
+	}
+
+	system("pause");
+
+}
+
